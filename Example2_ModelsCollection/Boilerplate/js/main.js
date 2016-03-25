0928234a311df1ec8b69f2302de3865e7dd1fd4a
dougc333 <dougc@stanfordalumni.org>
2016-03-25 12:51:08 -0700

    add

--- Example2_ModelsCollection/Boilerplate/js/main.js
+++
@@ -1,4 +0,0 @@
-
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
--- Example2_ModelsCollection/README.md
+++
@@ -1,5 +0,0 @@
-Collecdions. 
-
-
-bottle returns only a dict object from the POST method
-map this to GET/POST 
