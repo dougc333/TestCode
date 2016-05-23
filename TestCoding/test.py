73d1d326d1ce6ed9e3ec6bb69a673db7b7c2ef7e
dougc333 <dougc@stanfordalumni.org>
2016-05-23 13:49:03 -0700

    add

--- TestCoding/test.py
+++ TestCoding/test.py
@@ -1,4 +1,17 @@
-f=open('/Users/dc/TestCode/TestCoding/test.txt')
-for line in iter(lambda: f.readline(), ''):
-	print line
+import os
 
+f=open('/Users/dc/TestCode/TestCoding/lookup.csv')
+for line in iter(lambda: f.readline(), ''):
+	#print line
+	terms=line.split("|")
+	print terms[0], terms[1]
+	if os.path.exists("/Users/dc/TestCode/TestCoding/testindex/"+terms[1].strip()):
+		fappend=open("/Users/dc/TestCode/TestCoding/testindex/"+terms[1].strip(),"a")
+		fappend.write(terms[0]+"\n")
+		fappend.close()
+	else:
+		fwrite=open("/Users/dc/TestCode/TestCoding/testindex/"+terms[1].strip(),"w")
+		fwrite.write(terms[0]+"\n")
+		fwrite.close()
+
+f.close()
