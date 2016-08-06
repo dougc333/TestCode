f215cfda802a9fef6165f4e787711fb6f9c371bc
dougc333 <dougc@stanfordalumni.org>
2016-08-06 11:23:31 -0700

    add

--- TestNLP/Assignment1/arcs-pytest/test.py
+++ TestNLP/Assignment1/arcs-pytest/test.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import sys
+import csv
 
 class Config:
     def __init__(self):
@@ -9,25 +11,60 @@ class Config:
 
 
 def non_projective(conf):
-    print 'non_projective keys:', str(conf.heads.keys())
+    #print 'non_projective keys:', str(conf.heads.keys())
     for dep1 in conf.heads.keys():
         head1 = conf.heads[dep1]
         for dep2 in conf.heads.keys():
             head2 = conf.heads[dep2]
-            print 'processing head1:', str(head1), ' dep1:', str(dep1), " head2:", str(head2), " dep2:", str(dep2)
+            #print 'processing head1:', str(head1), ' dep1:', str(dep1), " head2:", str(head2), " dep2:", str(dep2)
             if head1 < 0 or head2 < 0:
                 continue
             if (dep1 > head2 and dep1 < dep2 and head1 < head2) or (dep1 < head2 and dep1 > dep2 and head1 < dep2):
-                print 'True1:dep1,dep2,head1,head2', dep1, dep2, head1, head2
+                #print 'True1:dep1,dep2,head1,head2', dep1, dep2, head1, head2
                 return True
 
             if dep1 < head1 and head1 is not head2:
                 if (head1 > head2 and head1 < dep2 and dep1 < head2) or (
                             head1 < head2 and head1 > dep2 and dep1 < dep2):
-                    print 'True2:dep1,dep2,head1,head2', dep1, dep2, head1, head2
+                    #print 'True2:dep1,dep2,head1,head2', dep1, dep2, head1, head2
                     return True
-    print 'false'
+    #print 'false'
     return False
+
+
+def init():
+
+    index=0
+    WORD=1
+    POS=3
+    HEAD=6
+    ALT_HEAD=7
+
+    # test for -1, not here
+    with open('/Users/dc/TestCode/TestNLP/Assignment1/arcs-pytest/sample-data/test.parses') as f:
+        reader = csv.reader(f, delimiter='\t')
+        for l in reader:
+            #print l, len(l)
+            if len(l)>0 and l[6]==-1:
+                print l
+
+    with open('/Users/dc/TestCode/TestNLP/Assignment1/arcs-pytest/sample-data/train.parses') as f:
+        reader = csv.reader(f, delimiter='\t')
+        sentences=[]
+        for l in reader:
+            # print l, len(l)
+            if len(l) > 0:
+                if l[1] == '?':
+                    print '----------'
+                    print l
+                    print '----------'
+                print l[WORD],l[POS],int(l[HEAD])-1,l[ALT_HEAD]
+                sentences.append((l[WORD],l[POS],int(l[HEAD])-1,l[ALT_HEAD]))
+                print 'len(sentences):',len(sentences)
+        return sentences
+
+def make_heads_deps(s[]):
+    
 
 if __name__ == '__main__':
     conf = Config()
@@ -36,4 +73,4 @@ if __name__ == '__main__':
     print str(conf.heads)
     #, str(conf.deps)
     print non_projective(conf)
-
+    init()
