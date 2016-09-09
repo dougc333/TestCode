62e5612ca555cae17d965269e5fd1c540ac8263b
dougc333 <dougc@stanfordalumni.org>
2016-09-09 15:47:28 -0700

    add

--- TestFlask/app/views.py
+++ TestFlask/app/views.py
@@ -6,3 +6,8 @@ from flask import render_template
 def index():
   user = {'nickname': 'Aaaa'} 
   return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/user')
+def user():
+  return render_template('user.html', title="User")
