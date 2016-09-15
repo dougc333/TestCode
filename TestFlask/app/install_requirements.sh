e1642ac3931ff3fb1562f37f2a84586ff3aa9bcf
dougc333 <dougc@stanfordalumni.org>
2016-09-15 12:55:10 -0700

    add

---
+++ TestFlask/app/install_requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-login
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-openid
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-mail
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-sqlalchemy
+/Users/dc/TestCode/TestFlask/flask/bin/pip install sqlalchemy-migrate
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-whooshalchemy
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-wtf
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flask-babel
+/Users/dc/TestCode/TestFlask/flask/bin/pip install guess_language
+/Users/dc/TestCode/TestFlask/flask/bin/pip install flipflop
+/Users/dc/TestCode/TestFlask/flask/bin/pip install coverage
