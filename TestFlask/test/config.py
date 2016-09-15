c9bfa097a14d59827ab5f61161943702cb44465b
dougc333 <dougc@stanfordalumni.org>
2016-09-15 12:55:10 -0700

    add

---
+++ TestFlask/test/config.py
@@ -0,0 +1,15 @@
+WFT_CRSF_ENABLED=True
+SECRET_KEY="a"
+
+#only upper case ids get passed into app.config dictionary
+TEST_LIST = [1,2,3,4]
+
+test = ['a','b','c']
+
+OPENID_PROVIDERS=[
+{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
