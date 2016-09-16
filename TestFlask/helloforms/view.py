07a94be1a2e24777a8878ea332491190c549fe95
dougc333 <dougc@stanfordalumni.org>
2016-09-16 11:15:47 -0700

    add

--- TestFlask/helloforms/view.py
+++ TestFlask/helloforms/view.py
@@ -1,18 +1,18 @@
 from flask import render_template,redirect, flash
 from helloforms import app
-from .form import LoginForm
+from .forms import LoginForm
 
 
-@app.route('/')
 @app.route('/index')
 def index():
-	render_template('index.html')
+	print 'calling index'
+	return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-	form=LoginForm()
+	form=LoginForm(csrf_enabled=False)
 	if form.validate_on_submit():
 		flash("openid='%s', remember_me='%s' ",(form.openid.data, str(form.remember_me.data)))
 		return redirect('index.html')
-	render_template('login.html',title="login", form=form, providers=app.config['OPENID_PROVIDERS'])
+	return render_template('login.html',title="login", form=form, providers=app.config['OPENID_PROVIDERS'])
